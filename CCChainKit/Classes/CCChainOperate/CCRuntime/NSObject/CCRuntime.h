179619cae6e90f55c3ed48f944becc910f557951
VArbiter <786292463@qq.com>
2017-08-18 10:35:32 +0800

    update and finish with 'CCRunTime' . for now .

--- CCChainKit/Classes/CCChainOperate/CCRuntime/NSObject/CCRuntime.h
+++ CCChainKit/Classes/CCChainOperate/CCRuntime/NSObject/CCRuntime.h
@@ -16,18 +16,32 @@ typedef NS_ENUM(unsigned long , CCAssociationPolicy) {
     CCAssociationPolicy_copy = 01403
 };
 
-typedef NS_ENUM(unsigned long , CCQueuePriority) {
-    CCQueuePriority_Default = 0,
-    CCQueuePriority_High = 2,
-    CCQueuePriority_Low = -2,
-    CCQueuePriority_Background = INT16_MIN 
+typedef NS_ENUM(unsigned long , CCQueueQOS) {
+#if __IPHONE_OS_VERSION_MAX_ALLOWED < __IPHONE_8_0
+    CCQueueQOS_Default = 0,
+    CCQueueQOS_High = 2,
+    CCQueueQOS_Low = -2,
+    CCQueueQOS_Background = INT16_MIN
+#else
+    CCQueueQOS_User_interaction = 0x21 , // user intercation , will finish as soon as possiable , DO NOT use it for large tasks !
+    CCQueueQOS_User_Initiated = 0x19 , // what's user expacted , DO NOT use it for large tasks !
+    CCQueueQOS_Default = 0x15 , // default , not for programmer . use it when you have to reset a serial tasks .
+    CCQueueQOS_Utility = 0x11 , // recommended , (also availiable for large tasks)
+    CCQueueQOS_Background = 0x09 , // background tasks .
+    CCQueueQOS_Unspecified = 0x00 // unspecified , wait unit the system to specific one .
+#endif
 };
 
 typedef dispatch_queue_t CCQueue;
 typedef dispatch_group_t CCGroup;
 typedef dispatch_source_t CCSource;
+typedef dispatch_time_t CCTime;
+typedef size_t CCCount;
 
 @interface CCRuntime : NSObject
+
+CCQueue CC_MAIN_QUEUE();
+
 /// absolute singleton
 @property (nonatomic , class , copy , readonly) CCRuntime *(^runtime)();
 
@@ -45,6 +59,10 @@ typedef dispatch_source_t CCSource;
 @property (nonatomic , copy , readonly) CCRuntime *(^async)(CCQueue queue , void (^)());
 /// sync to specific queue , warning : do NOT deploy it in MAIN QUEUE , other wise will cause lock done .
 @property (nonatomic , copy , readonly) CCRuntime *(^sync)(CCQueue queue , void (^)());
+/// equals to dispatch_barrier_async
+@property (nonatomic , copy , readonly) CCRuntime *(^barrierAsync)(CCQueue queue , void (^)());
+/// equals to dispatch_apply
+@property (nonatomic , copy , readonly) CCRuntime *(^applyFor)(CCCount count , CCQueue queue , void (^time)(CCCount t));
 /// equals to objc_setAssociatedObject
 @property (nonatomic , copy , readonly) CCRuntime *(^setAssociate)(id object , const void *key , id value , CCAssociationPolicy policy);
 /// equals to objc_getAssociatedObject
@@ -52,9 +70,61 @@ typedef dispatch_source_t CCSource;
 
 @end
 
+#pragma mark - -----
+
 @interface CCRuntime (CCChain_Queue)
 
 @property (nonatomic , class , copy , readonly) CCQueue (^createQ)(const char * label , BOOL isSerial);
-@property (nonatomic , class , copy , readonly) CCQueue (^global)()
+@property (nonatomic , class , copy , readonly) CCQueue (^global)(CCQueueQOS);
 
 @end
+
+#pragma mark - -----
+
+@protocol CCRunTimeGroupProtocol <NSObject>
+
+@property (nonatomic , strong) CCGroup group;
+@property (nonatomic , strong) CCQueue queue;
+
+@end
+
+@interface CCRuntime (CCChain_Group) < CCRunTimeGroupProtocol >
+
+CCGroup CC_GROUP_INIT();
+
+/// return a new object of CCRuntime , not the shared instance .
+@property (nonatomic , class , copy , readonly) CCRuntime *(^groupG)(CCGroup group , CCQueue queue);
+/// actions for group
+@property (nonatomic , copy , readonly) CCRuntime *(^groupAction)(void (^actions)());
+/// when all group actions finished
+@property (nonatomic , copy , readonly) CCRuntime *(^notifyG)(CCQueue queue , void(^finish)());
+
+/// enter and leave mast use it with a pair
+/// enter a group
+@property (nonatomic , copy , readonly) CCRuntime *(^enterG)();
+/// leave a group
+@property (nonatomic , copy , readonly) CCRuntime *(^leaveG)();
+/// do someting after delay .
+@property (nonatomic , copy , readonly) CCRuntime *(^waitG)(CCTime time);
+
+@end
+
+#pragma mark - -----
+
+@protocol CCChainClassProtocol <NSObject>
+
+/// for some properties that don't want be found .
++ (NSArray <NSString *> *) CCIgnores ;
+
+@end
+
+@interface CCRuntime (CCChain_Class)
+
+/// class that want to be found , <types , properties>
+@property (nonatomic , copy , readonly) CCRuntime *(^getIVar)(Class clazz , void (^finish)(NSDictionary <NSString * , NSString *> *dictionary));
+/// add a method with one argument .
+@property (nonatomic , copy , readonly) CCRuntime *(^addMethod)(Class clazz, NSString *selName , SEL impSupply) ;
+
+@end
+
+
--- CCChainKit/Classes/CCChainOperate/CCRuntime/NSObject/CCRuntime.m
+++ CCChainKit/Classes/CCChainOperate/CCRuntime/NSObject/CCRuntime.m
@@ -15,15 +15,27 @@
 
 @implementation CCRuntime
 
+CCQueue CC_MAIN_QUEUE() {
+    return dispatch_get_main_queue();
+}
+
 static CCRuntime *_instance = nil;
 + (CCRuntime *(^)())runtime {
     return ^CCRuntime * {
+        /*
         if (_instance) return _instance;
         _instance = [[CCRuntime alloc] init];
         return _instance;
+         */
+        if (_instance) return _instance;
+        static dispatch_once_t onceToken;
+        dispatch_once(&onceToken, ^{
+            _instance = [[CCRuntime alloc] init];
+        });
+        return _instance;
     };
 }
-
+/*
 + (instancetype)allocWithZone:(struct _NSZone *)zone {
     if (_instance) return _instance;
     static dispatch_once_t onceToken;
@@ -32,7 +44,7 @@ static CCRuntime *_instance = nil;
     });
     return _instance;
 }
-
+*/
 - (id)copyWithZone:(NSZone *)zone {
     return _instance;
 }
@@ -40,6 +52,7 @@ static CCRuntime *_instance = nil;
 - (id)mutableCopyWithZone:(NSZone *)zone {
     return _instance;
 }
+
 
 - (CCRuntime *(^)(SEL, SEL))swizz {
     __weak typeof(self) pSelf = self;
@@ -104,9 +117,7 @@ static CCRuntime *_instance = nil;
     __weak typeof(self) pSelf = self;
     return ^CCRuntime *(CCQueue q, void (^t)()) {
         if (!q) return pSelf;
-        dispatch_async(q, ^{
-            if (t) t();
-        });
+        dispatch_async(q ? q : CC_MAIN_QUEUE(), t);
         return pSelf;
     };
 }
@@ -114,9 +125,23 @@ static CCRuntime *_instance = nil;
     __weak typeof(self) pSelf = self;
     return ^CCRuntime *(CCQueue q, void (^t)()) {
         if (!q) return pSelf;
-        dispatch_sync(q, ^{
-            if (t) t();
-        });
+        dispatch_sync(q ? q : dispatch_queue_create("love.cc.love.home", NULL), t);
+        return pSelf;
+    };
+}
+
+- (CCRuntime *(^)(CCQueue, void (^)()))barrierAsync {
+    __weak typeof(self) pSelf = self;
+    return ^CCRuntime *(CCQueue q , void (^t)()) {
+        dispatch_barrier_async(q ? q : CC_MAIN_QUEUE(), t);
+        return pSelf;
+    };
+}
+
+- (CCRuntime *(^)(CCCount, CCQueue, void (^)(CCCount)))applyFor {
+    __weak typeof(self) pSelf = self;
+    return ^CCRuntime * (CCCount c , CCQueue q , void (^t)(CCCount)) {
+        dispatch_apply(c, q ? q : CC_MAIN_QUEUE(), t);
         return pSelf;
     };
 }
@@ -146,6 +171,125 @@ static CCRuntime *_instance = nil;
         if (UIDevice.currentDevice.systemVersion.floatValue >= 10.0) {
             return dispatch_queue_create(c, b ? NULL : DISPATCH_QUEUE_CONCURRENT_WITH_AUTORELEASE_POOL);
         } else return dispatch_queue_create(c, b ? NULL : DISPATCH_QUEUE_CONCURRENT);
+    };
+}
+
++ (CCQueue (^)(CCQueueQOS))global {
+    return ^CCQueue (CCQueueQOS qos){
+        /// for unsigned long flags , what's DOCs told that , it use for reserves for future needs .
+        /// thus , for now , it's always be 0 .
+        return dispatch_get_global_queue(qos, 0) ;
+    };
+}
+
+@end
+
+#pragma mark - -----
+
+@implementation CCRuntime (CCChain_Group)
+
+@dynamic group;
+@dynamic queue;
+
+CCGroup CC_GROUP_INIT() {
+    return dispatch_group_create();
+}
+
++ (CCRuntime *(^)(CCGroup, CCQueue))groupG {
+    return ^CCRuntime *(CCGroup g , CCQueue q) {
+        CCRuntime *r = CCRuntime.alloc.init;
+        r.group = g;
+        r.queue = q;
+        return r;
+    };
+}
+
+- (CCRuntime *(^)(void (^)()))groupAction {
+    __weak typeof(self) pSelf = self;
+    return ^CCRuntime * (void (^t)()) {
+        dispatch_group_async(pSelf.group, pSelf.queue, t);
+        return pSelf;
+    };
+}
+
+- (CCRuntime *(^)(CCQueue, void (^)()))notifyG {
+    __weak typeof(self) pSelf = self;
+    return ^CCRuntime *(CCQueue q, void (^t)()) {
+        dispatch_group_notify(pSelf.group, q, t);
+        return pSelf;
+    };
+}
+
+- (CCRuntime *(^)())enterG {
+    __weak typeof(self) pSelf = self;
+    return ^CCRuntime * {
+        dispatch_group_enter(pSelf.group);
+        return pSelf;
+    };
+}
+
+- (CCRuntime *(^)())leaveG {
+    __weak typeof(self) pSelf = self;
+    return ^CCRuntime * {
+        dispatch_group_leave(pSelf.group);
+        return pSelf;
+    };
+}
+
+- (CCRuntime *(^)(CCTime))waitG {
+    __weak typeof(self) pSelf = self;
+    return ^CCRuntime *(CCTime t){
+        dispatch_group_wait(pSelf.group, t);
+        return pSelf;
+    };
+}
+
+@end
+
+#pragma mark - -----
+
+@implementation CCRuntime (CCChain_Class)
+
+- (CCRuntime *(^)(__unsafe_unretained Class, void (^)(NSDictionary<NSString *,NSString *> *)))getIVar {
+    __weak typeof(self) pSelf = self;
+    return ^CCRuntime * (Class c , void (^t)(NSDictionary<NSString *,NSString *> *)) {
+        unsigned int iVars = 0;
+        Ivar *ivarList = class_copyIvarList(c, &iVars);
+        
+        NSMutableDictionary <NSString * , NSString *> * dictionary = [NSMutableDictionary dictionary];
+        for (unsigned int i = 0 ; i < iVars; i ++) {
+            // get type and property names
+            NSString *sMember = [NSString stringWithUTF8String:ivar_getName(ivarList[i])]; // output with _ , eg:_cc
+            if ([sMember hasPrefix:@"_"]) {
+                sMember = [sMember substringFromIndex:1];
+            }
+            // get ignores
+            if ([c respondsToSelector:NSSelectorFromString(@"CCIgnores")]) {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+                NSArray *a = [c performSelector:NSSelectorFromString(@"CCIgnores")];
+#pragma clang diagnostic pop
+                if ([a containsObject:sMember]) continue;
+            }
+            
+            NSString *sMemberType = [NSString stringWithUTF8String:ivar_getTypeEncoding(ivarList[i])]; // output @"NSString"
+            sMemberType = [sMemberType stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"@\""]]; // compress
+            [dictionary setValue:sMemberType // types can be found multi times , therefore , make it values .
+                          forKey:sMember];
+        }
+        if (t) t(dictionary);
+        return pSelf;
+    };
+}
+
+- (CCRuntime *(^)(__unsafe_unretained Class, NSString *, SEL))addMethod {
+    __weak typeof(self) pSelf = self;
+    return ^(Class c , NSString *s , SEL sel) {
+        class_addMethod(c,
+                        NSSelectorFromString(s),
+                        class_getMethodImplementation(c, sel),
+                        "s@:@");
+        return pSelf;
     };
 }
 
